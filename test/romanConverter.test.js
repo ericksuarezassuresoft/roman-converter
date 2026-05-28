d9658b9e478ec8b71d272971041c90215b8494a7
erick.suarez <erick.suarez@webpt.com>
2026-05-28 10:34:44 -0400

    Cycle 2, red

--- test/romanConverter.test.js
+++ test/romanConverter.test.js
@@ -3,3 +3,7 @@ const romanConverter = require("../src/romanConverter");
 it("It shoul return I ", () => {
     expect(romanConverter(1)).toBe("I");
 });
+
+it("It shoul return II ", () => {
+    expect(romanConverter(2)).toBe("II");
+});
