85aafb1a3739cdb442ab79ec72d3dbd74927b1dc
erick.suarez <erick.suarez@webpt.com>
2026-05-28 10:46:06 -0400

    Cycle 5, refactor

--- src/romanConverter.js
+++ src/romanConverter.js
@@ -2,10 +2,11 @@ function romanConverter(roman) {
     if (roman === 5) {
         return "V";
     }
-    
+
     if (roman === 4) {
         return "IV";
     }
+    
     return 'I'.repeat(roman);
 }
 
--- test/romanConverter.test.js
+++ test/romanConverter.test.js
@@ -1,21 +1,21 @@
 const romanConverter = require("../src/romanConverter");
 
-it("It shoul return I ", () => {
+it("It should return I", () => {
     expect(romanConverter(1)).toBe("I");
 });
 
-it("It shoul return II ", () => {
+it("It should return II", () => {
     expect(romanConverter(2)).toBe("II");
 });
 
-it("It shoul return III ", () => {
+it("It should return III", () => {
     expect(romanConverter(3)).toBe("III");
 });
 
-it("It shoul return IV ", () => {
+it("It should return IV", () => {
     expect(romanConverter(4)).toBe("IV");
 });
 
-it("It shoul return V ", () => {
+it("It should return V", () => {
     expect(romanConverter(5)).toBe("V");
 });
