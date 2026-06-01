b037addf5d40e9f5bc4afa1090ac4497ee55b346
erick.suarez <erick.suarez@webpt.com>
2026-06-01 15:25:49 -0400

    Refactor facade

--- src/run.js
+++ src/run.js
@@ -1,9 +1,11 @@
-const romanConverter = require("./integerToRoman");
+const integerToRoman = require("./integerToRoman");
+const romanToInteger = require("./romanToInteger");
 
 const input = process.argv[2];
+const number = Number(input);
 
-if (typeof input === 'number') {
-    console.log("Output:", integerToRoman(input));
+if (Number.isInteger(number) && input.trim() !== '') {
+    console.log("Output:", integerToRoman(number));
 } else {
     console.log("Output:", romanToInteger(input));
 }
